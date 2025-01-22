6d1c7f573f242dc846da0be8cdffc03407039bef
Sean Zhang <sean@manifestai.co>
2025-01-21 23:41:53 -0500

    no more factory, just a wrapper

--- power_attention/power_full.py
+++ power_attention/power_full.py
@@ -119,128 +119,128 @@ References:
         Manifest AI, Aug. 15, 2024.
 """
 
-class PowerFullFactory:
-    @staticmethod
-    def pick_impl(impl: Enum):
-        if impl not in IMPL_MAP:
-            raise ValueError(f"Invalid implementation: {impl}")
-        return IMPL_MAP[impl]
+def pick_impl(impl: Enum):
+    """ Given an implementation enum, return the corresponding implementation function.
+    """
+    if impl not in IMPL_MAP:
+        raise ValueError(f"Invalid implementation: {impl}")
+    return IMPL_MAP[impl]
 
-    @staticmethod
-    def make_power_full(update_state_impl: UpdateStateImpl, query_state_impl: QueryStateImpl, discumsum_impl: DiscumsumImpl, attention_impl: AttentionImpl):
+def make_power_full(update_state_impl: UpdateStateImpl, query_state_impl: QueryStateImpl, discumsum_impl: DiscumsumImpl, attention_impl: AttentionImpl):
+    """ Create a power_full function with the given implementations.
+    """
+    def _power_full(Q, K, V, log_G=None, initial_state=None,
+                deg=2, scale=None, chunk_size=None): # noqa: C901
+        if initial_state is not None:
+            raise NotImplementedError('Initial state not implemented')
 
-        def _power_full(Q, K, V, log_G=None, initial_state=None,
-                    deg=2, scale=None, chunk_size=None): # noqa: C901
-            if initial_state is not None:
-                raise NotImplementedError('Initial state not implemented')
+        if deg == 1: # when deg == 1, update_state and query_state are essentially matmuls
+            _update_state = pick_impl(UpdateStateImpl.MATMUL)
+            _query_state = pick_impl(QueryStateImpl.MATMUL)
+        else:
+            _update_state = pick_impl(update_state_impl)
+            _query_state = pick_impl(query_state_impl)
+        _discumsum = pick_impl(discumsum_impl)
+        _attention = pick_impl(attention_impl)
+        
+        # Establish shapes and dtypes
+        assert Q.dtype == K.dtype == V.dtype, 'dtypes of inputs must match'
+        dtype = Q.dtype
+        b, t, hq, d = Q.shape
+        _, _, h, _ = K.shape
+        assert hq % h == 0, f"Q heads must be a multiple of KV heads: {hq=} {h=}"
+        qhead_ratio = hq // h
+        if chunk_size is not None:
+            c = chunk_size
+            assert t % chunk_size == 0, f'{t=} not evenly divisible by {chunk_size=}'
+            n = t // chunk_size
+        else:
+            c = t
+            n = 1
+        gating = log_G is not None
+        if gating:
+            log_G = log_G.to(torch.float32)
 
-            if deg == 1: # when deg == 1, update_state and query_state are essentially matmuls
-                _update_state = PowerFullFactory.pick_impl(UpdateStateImpl.MATMUL)
-                _query_state = PowerFullFactory.pick_impl(QueryStateImpl.MATMUL)
-            else:
-                _update_state = PowerFullFactory.pick_impl(update_state_impl)
-                _query_state = PowerFullFactory.pick_impl(query_state_impl)
-            _discumsum = PowerFullFactory.pick_impl(discumsum_impl)
-            _attention = PowerFullFactory.pick_impl(attention_impl)
-            
-            # Establish shapes and dtypes
-            assert Q.dtype == K.dtype == V.dtype, 'dtypes of inputs must match'
-            dtype = Q.dtype
-            b, t, hq, d = Q.shape
-            _, _, h, _ = K.shape
-            assert hq % h == 0, f"Q heads must be a multiple of KV heads: {hq=} {h=}"
-            qhead_ratio = hq // h
-            if chunk_size is not None:
-                c = chunk_size
-                assert t % chunk_size == 0, f'{t=} not evenly divisible by {chunk_size=}'
-                n = t // chunk_size
-            else:
-                c = t
-                n = 1
-            gating = log_G is not None
-            if gating:
-                log_G = log_G.to(torch.float32)
+        if not scale:
+            scale = 1.0 / d**0.5
 
-            if not scale:
-                scale = 1.0 / d**0.5
-
-            # Quick return for simple quadratic attention
-            if t <= c:
-                if qhead_ratio > 1:
-                    K = K.repeat_interleave(qhead_ratio, dim=2)
-                    V = V.repeat_interleave(qhead_ratio, dim=2)
-                    if gating:
-                        log_G = log_G.repeat_interleave(qhead_ratio, dim=2)
-                log_G_accum = log_G.cumsum(1) if log_G is not None else None
-                Y, _, _ = _attention(Q, K, V, log_G_accum, deg, scale)
-                assert Y.is_contiguous(), 'Y must be contiguous'
-                return Y
-
-            # Reshape into chunks
-            Q = Q.view(b, n, c, hq, d)
-            K = K.view(b, n, c, h, d)
-            V = V.view(b, n, c, h, d)    
-            if gating:
-                log_G = log_G.view(b, n, c, h).to(torch.float32)
-                log_G_intrachunk_accum = log_G.cumsum(2)
-
-            # Compute chunk states
-            if gating:
-                log_discount_weights = (log_G_intrachunk_accum.narrow(2, c-1, 1) - log_G_intrachunk_accum) / deg
-                cs_K = K * torch.exp(log_discount_weights).unsqueeze(-1).to(K.dtype)
-            else:
-                cs_K = K
-            S = _update_state(cs_K.contiguous(), V.contiguous(), deg)
-
-            # Accumulate
-            if gating:
-                log_G_chunk_sum = log_G_intrachunk_accum[:,:,-1].contiguous()
-            else:
-                log_G_chunk_sum = torch.zeros(size=(b, n, h), device=Q.device, dtype=torch.float32)
-            S = _discumsum(S, log_G_chunk_sum) # Note that this adds an empty chunk to the start of the sequence
-            S = S.narrow(1, 0, n)
-
-            # Restrict to non-initial chunks for recurrent outputs
-            Q = Q.contiguous()
-            K = K.contiguous()
-            V = V.contiguous()
-            if gating:
-                log_G_intrachunk_accum = log_G_intrachunk_accum.contiguous()
-
-            # Compute attention
-            Q_flatbatch = Q.view(b*n, c, hq, d)
-            K_flatbatch = K.view(b*n, c, h, d)
-            V_flatbatch = V.view(b*n, c, h, d)    
-            log_G_intrachunk_accum_flatbatch = log_G_intrachunk_accum.view(b*n, c, h) if gating else None
+        # Quick return for simple quadratic attention
+        if t <= c:
             if qhead_ratio > 1:
-                K_flatbatch = K_flatbatch.repeat_interleave(qhead_ratio, dim=2)
-                V_flatbatch = V_flatbatch.repeat_interleave(qhead_ratio, dim=2)
+                K = K.repeat_interleave(qhead_ratio, dim=2)
+                V = V.repeat_interleave(qhead_ratio, dim=2)
                 if gating:
-                    log_G_intrachunk_accum_flatbatch = log_G_intrachunk_accum_flatbatch.repeat_interleave(qhead_ratio, dim=2)
-            attn_Y, _, rowmax = _attention(Q_flatbatch, K_flatbatch, V_flatbatch, log_G_intrachunk_accum_flatbatch, deg, scale)
-            attn_Y = attn_Y.view(b, n, c, hq, d)
-            rowmax = rowmax.view(b, n, c, hq).detach()
-            rowmax = rowmax - math.log(scale)
+                    log_G = log_G.repeat_interleave(qhead_ratio, dim=2)
+            log_G_accum = log_G.cumsum(1) if log_G is not None else None
+            Y, _, _ = _attention(Q, K, V, log_G_accum, deg, scale)
+            assert Y.is_contiguous(), 'Y must be contiguous'
+            return Y
 
+        # Reshape into chunks
+        Q = Q.view(b, n, c, hq, d)
+        K = K.view(b, n, c, h, d)
+        V = V.view(b, n, c, h, d)    
+        if gating:
+            log_G = log_G.view(b, n, c, h).to(torch.float32)
+            log_G_intrachunk_accum = log_G.cumsum(2)
+
+        # Compute chunk states
+        if gating:
+            log_discount_weights = (log_G_intrachunk_accum.narrow(2, c-1, 1) - log_G_intrachunk_accum) / deg
+            cs_K = K * torch.exp(log_discount_weights).unsqueeze(-1).to(K.dtype)
+        else:
+            cs_K = K
+        S = _update_state(cs_K.contiguous(), V.contiguous(), deg)
+
+        # Accumulate
+        if gating:
+            log_G_chunk_sum = log_G_intrachunk_accum[:,:,-1].contiguous()
+        else:
+            log_G_chunk_sum = torch.zeros(size=(b, n, h), device=Q.device, dtype=torch.float32)
+        S = _discumsum(S, log_G_chunk_sum) # Note that this adds an empty chunk to the start of the sequence
+        S = S.narrow(1, 0, n)
+
+        # Restrict to non-initial chunks for recurrent outputs
+        Q = Q.contiguous()
+        K = K.contiguous()
+        V = V.contiguous()
+        if gating:
+            log_G_intrachunk_accum = log_G_intrachunk_accum.contiguous()
+
+        # Compute attention
+        Q_flatbatch = Q.view(b*n, c, hq, d)
+        K_flatbatch = K.view(b*n, c, h, d)
+        V_flatbatch = V.view(b*n, c, h, d)    
+        log_G_intrachunk_accum_flatbatch = log_G_intrachunk_accum.view(b*n, c, h) if gating else None
+        if qhead_ratio > 1:
+            K_flatbatch = K_flatbatch.repeat_interleave(qhead_ratio, dim=2)
+            V_flatbatch = V_flatbatch.repeat_interleave(qhead_ratio, dim=2)
             if gating:
-                if qhead_ratio > 1:
-                    log_G_intrachunk_accum = log_G_intrachunk_accum.repeat_interleave(qhead_ratio, dim=3)
-                Q = Q * torch.exp(log_G_intrachunk_accum / deg).unsqueeze(-1).to(Q.dtype)
+                log_G_intrachunk_accum_flatbatch = log_G_intrachunk_accum_flatbatch.repeat_interleave(qhead_ratio, dim=2)
+        attn_Y, _, rowmax = _attention(Q_flatbatch, K_flatbatch, V_flatbatch, log_G_intrachunk_accum_flatbatch, deg, scale)
+        attn_Y = attn_Y.view(b, n, c, hq, d)
+        rowmax = rowmax.view(b, n, c, hq).detach()
+        rowmax = rowmax - math.log(scale)
+
+        if gating:
             if qhead_ratio > 1:
-                S = S.repeat_interleave(qhead_ratio, dim=2)
-            D = float(compute_expanded_dim(d, deg))
-            Y = _query_state(Q.contiguous(), S.contiguous(), attn_Y.contiguous(), rowmax.contiguous(), deg, D, initial_state is None)
+                log_G_intrachunk_accum = log_G_intrachunk_accum.repeat_interleave(qhead_ratio, dim=3)
+            Q = Q * torch.exp(log_G_intrachunk_accum / deg).unsqueeze(-1).to(Q.dtype)
+        if qhead_ratio > 1:
+            S = S.repeat_interleave(qhead_ratio, dim=2)
+        D = float(compute_expanded_dim(d, deg))
+        Y = _query_state(Q.contiguous(), S.contiguous(), attn_Y.contiguous(), rowmax.contiguous(), deg, D, initial_state is None)
 
-            # Epilogue
-            out = Y.contiguous().view(b, t, hq, d).to(dtype)
-            return out
+        # Epilogue
+        out = Y.contiguous().view(b, t, hq, d).to(dtype)
+        return out
 
-        _power_full.__doc__ = POWER_FULL_DOC
-        return _power_full
+    _power_full.__doc__ = POWER_FULL_DOC
+    return _power_full
 
-power_full_reference = PowerFullFactory.make_power_full(UpdateStateImpl.REFERENCE, QueryStateImpl.REFERENCE, DiscumsumImpl.REFERENCE, AttentionImpl.REFERENCE)
+power_full_reference = make_power_full(UpdateStateImpl.REFERENCE, QueryStateImpl.REFERENCE, DiscumsumImpl.REFERENCE, AttentionImpl.REFERENCE)
 
-power_full = PowerFullFactory.make_power_full(UpdateStateImpl.CUTLASS, QueryStateImpl.CUTLASS, DiscumsumImpl.CUTLASS, AttentionImpl.CUTLASS)
+power_full = make_power_full(UpdateStateImpl.CUTLASS, QueryStateImpl.CUTLASS, DiscumsumImpl.CUTLASS, AttentionImpl.CUTLASS)
 
 ## Useful function to create sample inputs
 def create_inputs(b=2, t=1024, h=8, d=32, qhead_ratio=1, dtype=torch.float16, device='cuda', gating=False,
