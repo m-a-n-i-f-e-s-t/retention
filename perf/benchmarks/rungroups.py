da28b65e58a7f2e5f4681fa2324174ef46d4c0ec
Sean Zhang <sean@manifestai.co>
2025-02-10 12:37:07 -0500

    change rungroup type hints

--- perf/benchmarks/rungroups.py
+++ perf/benchmarks/rungroups.py
@@ -1,44 +1,44 @@
 """ This module contains the definition of run groups, which are groups of runs that share the same input variation. """
-from typing import List, Tuple
+from typing import Tuple, Iterator, Callable, Any
 from .runs import SDPA, FLA, Power, PowerTriton, TritonExpansion, MosaicExpansion
 
 
-def sdpa_context_scaling(params_range: List[Tuple[int, int]], **kw):
+def sdpa_context_scaling(params_range: Iterator[Tuple[int, int]], **kw) -> Iterator[Callable[[], Any]]:
     for b, ctx in params_range:
         yield SDPA.make_run(b=b, t=ctx, **kw)
 
-def sdpa_headdim_scaling(params_range: List[Tuple[int, int]], **kw):
+def sdpa_headdim_scaling(params_range: Iterator[int], **kw) -> Iterator[Callable[[], Any]]:
     for d in params_range:
         yield SDPA.make_run(d=d, **kw)
     
-def fla_context_scaling(params_range: List[Tuple[int, int]], **kw):
+def fla_context_scaling(params_range: Iterator[Tuple[int, int]], **kw) -> Iterator[Callable[[], Any]]:
     for b, ctx in params_range:
         yield FLA.make_run(b=b, t=ctx, **kw)
 
-def fla_headdim_scaling(params_range: List[Tuple[int, int]], **kw):
+def fla_headdim_scaling(params_range: Iterator[Tuple[int, int]], **kw) -> Iterator[Callable[[], Any]]:
     for d in params_range:
         yield FLA.make_run(d=d, **kw)
 
-def power_context_scaling(params_range: List[Tuple[int, int]], **kw):
+def power_context_scaling(params_range: Iterator[Tuple[int, int]], **kw) -> Iterator[Callable[[], Any]]:
     for b, ctx in params_range:
         yield Power.make_run(b=b, t=ctx, **kw)
 
-def power_headdim_scaling(params_range: List[Tuple[int, int]], **kw):
+def power_headdim_scaling(params_range: Iterator[int], **kw) -> Iterator[Callable[[], Any]]:
     for d in params_range:
         yield Power.make_run(d=d, **kw)
 
-def power_triton_context_scaling(params_range: List[Tuple[int, int]], **kw):
+def power_triton_context_scaling(params_range: Iterator[Tuple[int, int]], **kw) -> Iterator[Callable[[], Any]]:
     for b, ctx in params_range:
         yield PowerTriton.make_run(b=b, t=ctx, **kw)
 
-def power_triton_headdim_scaling(params_range: List[Tuple[int, int]], **kw):
+def power_triton_headdim_scaling(params_range: Iterator[int], **kw) -> Iterator[Callable[[], Any]]:
     for d in params_range:
         yield PowerTriton.make_run(d=d, **kw)
 
-def mosaic_expansion_context_scaling(t_range: List[int], **kw):
+def mosaic_expansion_context_scaling(t_range: Iterator[int], **kw) -> Iterator[Callable[[], Any]]:
     for t in t_range:
         yield MosaicExpansion.make_run(t=t, **kw)
 
-def triton_expansion_context_scaling(t_range: List[int], **kw):
+def triton_expansion_context_scaling(t_range: Iterator[int], **kw) -> Iterator[Callable[[], Any]]:
     for t in t_range:
         yield TritonExpansion.make_run(t=t, **kw)
